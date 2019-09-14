3c1638486f3c69ec7cd5fef06f8c83c13840211c
kim (grufwub) <grufwub@gmail.com>
2019-09-14 16:31:13 +0000

    final update script modifications
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,11 +7,10 @@ check_update_env() {
   [ -z "$BONCLI_GITURL" ]    && return 1
   [ -z "$BONCLI_GITBRANCH" ] && return 1
   TEMP_DIR=$(get_temp_dir)   || return 1
-  return 0
 }
 
 download_files() {
-  local url="$BONCLI_GIT/raw/$BONCLI_GITBRANCH"
+  local url="$BONCLI_GITURL/raw/$BONCLI_GITBRANCH"
 
   TEMP_BONCLI="$TEMP_DIR/boncli"
   TEMP_FILEPICKER="$TEMP_DIR/file_picker.py"
@@ -19,29 +18,31 @@ download_files() {
 
   printf 'downloading files...\n'
 
-  printf '-> latest boncli script\n'
+  printf '%s\n' '-> latest boncli script'
   download "$TEMP_BONCLI" "$url/boncli" || return 1
 
-  printf '-> latest file_picker.py script\n'
+  printf '%s\n' '-> latest file_picker.py script'
   download "$TEMP_FILEPICKER" "$url/scripts/file_picker.py" || return 1
 
-  printf '-> latest boncli_git.sh script\n'
+  printf '%s\n' '-> latest boncli_git.sh script'
   download "$TEMP_BONCLIGIT" "$url/scripts/boncli_git.sh" || return 1
 }
 
 replace_files() {
   local boncli_path
 
-  [ ! -f "$TEMP_BONCLI" ]     && return 1
+  [ ! -f "$TEMP_BONCLI" ] && return 1
   [ ! -f "$TEMP_FILEPICKER" ] && return 1
-  [ ! -f "$TEMP_BONCLIGIT" ]  && return 1
+  [ ! -f "$TEMP_BONCLIGIT" ] && return 1
 
   boncli_path=$(which boncli)
   if [ ! -w "$BONCLI_PATH" ]; then
     # Path not writeable, try sudo, return if fail
-    sudo -E mv "$TEMP_BONCLI" "$boncli_path" > /dev/null 2>&1 || return 1
+    sudo -E mv "$TEMP_BONCLI" "$boncli_path" || return 1
+    sudo -E chmod +x "$boncli_path"
   else
-    mv "$TEMP_BONCLI" "$boncli_path" > /dev/null 2>&1         || return 1
+    mv "$TEMP_BONCLI" "$boncli_path" || return 1
+    chmod +x "$boncli_path"
   fi
 
   mv "$TEMP_FILEPICKER" "$FILE_PICKER" > /dev//null 2>&1 || return 1
@@ -50,10 +51,10 @@ replace_files() {
 
 printf 'updater script:\n'
 
-#if ( ! check_update_env ); then
-#  printf 'environment check failed...\n'
-#  return 1
-#fi
+if ( ! check_update_env ); then
+  printf 'environment check failed...\n'
+  return 1
+fi
 
 check_update_env
 download_files || return 1
